d36f97cff55cd306957b88056da92a8a9ad08858
alex2cloud <dattran.it@outlook.com>
2025-03-23 14:54:39 +0700

    Update install-nfs-provisioner.sh

--- bootstrap-cluster/persistentVolume/install-nfs-provisioner.sh
+++ bootstrap-cluster/persistentVolume/install-nfs-provisioner.sh
@@ -2,4 +2,6 @@
 NFS_SERVER=192.168.116.128
 NFS_PATH="/var/nfs/data"
 sudo snap install helm --classic
+helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
+helm repo update
 helm -n  nfs-provisioner install nfs-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner --create-namespace --set nfs.server=${NFS_SERVER} --set nfs.path=${NFS_PATH} --set storageClass.defaultClass=true --set replicaCount=1 --set storageClass.name=nfs --set storageClass.provisionerName=nfs-provisioner  
