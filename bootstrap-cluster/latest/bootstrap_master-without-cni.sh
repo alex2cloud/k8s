9526e9328258d42f8b557dcb2baabf9f87474991
a2c <dattran.it@outlook.com>
2025-08-01 14:00:31 +0700

    Update bootstrap_master-without-cni.sh

--- bootstrap-cluster/latest/bootstrap_master-without-cni.sh
+++ bootstrap-cluster/latest/bootstrap_master-without-cni.sh
@@ -400,3 +400,10 @@ sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
 echo
 echo "### COMMAND TO ADD A WORKER NODE ###"
 kubeadm token create --print-join-command --ttl 0
+
+### kubectl completion
+kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
+sudo chmod a+r /etc/bash_completion.d/kubectl
+echo 'alias k=kubectl' >>~/.bashrc
+echo 'complete -o default -F __start_kubectl k' >>~/.bashrc
+source ~/.bashrc
