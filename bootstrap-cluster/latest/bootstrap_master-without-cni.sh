0cbe37df1d10a326629a0795846d437a82857648
a2c <dattran.it@outlook.com>
2025-08-01 16:11:58 +0700

    Update bootstrap_master-without-cni.sh

--- bootstrap-cluster/latest/bootstrap_master-without-cni.sh
+++ bootstrap-cluster/latest/bootstrap_master-without-cni.sh
@@ -392,18 +392,3 @@ systemctl enable kubelet && systemctl start kubelet
 ### init k8s
 rm /root/.kube/config || true
 kubeadm init --kubernetes-version=${KUBE_VERSION} --ignore-preflight-errors=NumCPU --skip-token-print --pod-network-cidr 192.168.0.0/16
-
-mkdir -p ~/.kube
-sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
-
-### finished
-echo
-echo "### COMMAND TO ADD A WORKER NODE ###"
-kubeadm token create --print-join-command --ttl 0
-
-### kubectl completion
-kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
-sudo chmod a+r /etc/bash_completion.d/kubectl
-echo 'alias k=kubectl' >>~/.bashrc
-echo 'complete -o default -F __start_kubectl k' >>~/.bashrc
-source ~/.bashrc
